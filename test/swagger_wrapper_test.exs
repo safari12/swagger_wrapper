89c7daf1e6d5a9fe66baa7827149cdb653d3a8ca
safari <rsafari.s@gmail.com>
2020-11-13 09:09:41 -0600

    unit test generated wrapper functions

--- test/swagger_wrapper_test.exs
+++ test/swagger_wrapper_test.exs
@@ -5,11 +5,25 @@ defmodule SwaggerWrapperTest do
 
   alias Http.Mock
 
-  test "should generate ping" do
-    expect(Mock, :get, fn _url ->
-      {:ok, %HTTPoison.Response{status_code: 200, body: Poison.encode!(%{message: "pong"})}}
-    end)
+  describe "should generate wrapper functions" do
+    test "test with arity 1" do
+      assert Kernel.function_exported?(TestWrapper, :test, 1)
+    end
 
-    assert {:ok, %{body: %{"message" => "pong"}}} = TestWrapper.ping([])
+    test "test_hello with arity 2" do
+      assert Kernel.function_exported?(TestWrapper, :test_hello, 2)
+    end
+
+    test "test_hello_world with arity 3" do
+      assert Kernel.function_exported?(TestWrapper, :test_hello_world, 3)
+    end
   end
+
+  # test "should generate ping" do
+  #   expect(Mock, :get, fn _url ->
+  #     {:ok, %HTTPoison.Response{status_code: 200, body: Poison.encode!(%{message: "pong"})}}
+  #   end)
+
+  #   assert {:ok, %{body: %{"message" => "pong"}}} = TestWrapper.test([])
+  # end
 end
--- test/test_wrapper.ex
+++ test/test_wrapper.ex
@@ -1,5 +1,5 @@
 defmodule TestWrapper do
   use SwaggerWrapper,
-    filepath: "coingecko_swagger.json",
+    filepath: "test/assets/test_swagger.json",
     http_adapter: Application.get_env(:swagger_wrapper, :http_adapter)
 end
