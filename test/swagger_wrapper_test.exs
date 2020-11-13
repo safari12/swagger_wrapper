390e158fdf3800d6f1e0bb7cb015093f474d059e
safari <rsafari.s@gmail.com>
2020-11-13 12:42:14 -0600

    added unit testing for http calls

--- test/swagger_wrapper_test.exs
+++ test/swagger_wrapper_test.exs
@@ -3,7 +3,15 @@ defmodule SwaggerWrapperTest do
 
   import Mox
 
-  alias Http.Mock
+  @base_url "https://api.example.com/api/v3"
+
+  defp sample_http_response do
+    {:ok, %HTTPoison.Response{status_code: 200, body: Poison.encode!(%{message: "hello world"})}}
+  end
+
+  defp expected_body do
+    %{"message" => "hello world"}
+  end
 
   describe "should generate wrapper functions" do
     test "test with arity 1" do
@@ -19,11 +27,32 @@ defmodule SwaggerWrapperTest do
     end
   end
 
-  # test "should generate ping" do
-  #   expect(Mock, :get, fn _url ->
-  #     {:ok, %HTTPoison.Response{status_code: 200, body: Poison.encode!(%{message: "pong"})}}
-  #   end)
+  describe "should create http request" do
+    test "test" do
+      expect(Http.Mock, :get, fn @base_url <> "/test" ->
+        sample_http_response()
+      end)
 
-  #   assert {:ok, %{body: %{"message" => "pong"}}} = TestWrapper.test([])
-  # end
+      assert {:ok, response} = TestWrapper.test([])
+      assert expected_body() == response.body
+    end
+
+    test "test_hello" do
+      expect(Http.Mock, :get, fn @base_url <> "/test/1/hello" ->
+        sample_http_response()
+      end)
+
+      assert {:ok, response} = TestWrapper.test_hello("1", [])
+      assert expected_body() == response.body
+    end
+
+    test "test_hello_world" do
+      expect(Http.Mock, :get, fn @base_url <> "/test/1/hello/blah/world" ->
+        sample_http_response()
+      end)
+
+      assert {:ok, response} = TestWrapper.test_hello_world("1", "blah", [])
+      assert expected_body() == response.body
+    end
+  end
 end
